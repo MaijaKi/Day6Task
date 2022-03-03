dca8bce182a4b0cf1713c01510a91ef709c0810e
Maija Kikule <maija.kikule@gmail.com>
2022-03-03 20:19:59 +0200

    First couple of tasks are done.

---
+++ Day6FirstTask/Day6FirstTask/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+short A = 14;
+short B = 2;
+
+if (A > B)
+{ Console.WriteLine($"{A} is greater than {B}"); }
+
+if (B > A)
+{ Console.WriteLine($"{B} is greater than {A}"); }
+
