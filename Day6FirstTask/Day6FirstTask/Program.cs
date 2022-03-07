0e0a778126be92df0c72e830714bd774224118d8
Maija Kikule <maija.kikule@gmail.com>
2022-03-07 18:14:10 +0200

    HomeWork

--- Day6FirstTask/Day6FirstTask/Program.cs
+++ Day6FirstTask/Day6FirstTask/Program.cs
@@ -59,7 +59,7 @@ Console.WriteLine(output);
 
 Console.WriteLine("******************************************");
 
-var time = -2;
+var time = 8;
 var day = 12;
 var month = 5;
 var year = 2021;
@@ -84,3 +84,45 @@ if (year < 0)
 { Console.WriteLine("Please check the year!"); }
 
 Console.WriteLine($"{year}/{month}/{day}");
+
+Console.WriteLine("******************************************");
+Console.WriteLine("Please write the day!");
+string dayString = Console.ReadLine();
+Console.WriteLine("Please write the month!");
+string monthString = Console.ReadLine(); 
+Console.WriteLine("Please write the year!");
+string yearString = Console.ReadLine();
+
+day = Convert.ToInt32(dayString);
+month = Convert.ToInt32(monthString);
+year = Convert.ToInt32(yearString);
+
+Console.WriteLine($"Please select date format: press '1' for YYYY/MM/DD  or press '2' for DD.MM.YYYY");
+string format = Console.ReadLine();
+
+Console.WriteLine("***********************************************************");
+if ((month == 4 || month == 6 || month == 9 || month == 11) && (day < 1 || day > 30))
+{ Console.WriteLine("Please check the date - it's not correct!"); }
+
+if ((month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12) && (day < 1 || day > 31))
+{ Console.WriteLine("Please check the date - it's not correct!"); }
+
+if (month == 2 && (day < 1 || day > 28) && (year % 4 != 0))
+{ Console.WriteLine("Please check the day - it's not correct!"); }
+
+if (month == 2 && (day < 1 || day > 29) && (year % 4 == 0))
+{ Console.WriteLine("Please check the day - it's not correct!"); }
+
+if (month < 1 || month > 12)
+{ Console.WriteLine("Please check the month - it's not correct!"); }
+
+if (year < 0)
+{ Console.WriteLine("Please check the year - it's not correct!"); }
+
+
+if (format == "1")
+    { Console.WriteLine($"Your date is {year}/{month}/{day}"); }
+     else if (format == "2")
+     { Console.WriteLine($"Your date is {day}.{month}.{year}"); }
+
+Console.WriteLine("***********************************************************");
